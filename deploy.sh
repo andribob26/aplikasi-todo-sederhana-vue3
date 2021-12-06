41b9276f4b59d30e619ae6bb19da9dee2091cf0b
andribob26 <andri.feb.26@gmail.com>
2021-12-07 01:49:56 +0700

    sedond

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:andribob26/aplikasi-todo-sederhana-vue3.git master:gh-pages
+git push -f git@github.com:andribob26/aplikasi-todo-sederhana-vue3.git main:gh-pages
 
 cd -
